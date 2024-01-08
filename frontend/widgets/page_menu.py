6e940965871c7b4b0267460ce9c9ab66f9f4c552
mikk583d <mikkelheienlauridsen@gmail.com>
2024-01-08 13:01:41 +0100

    Fixed title_menu and page_menu widgets

--- frontend/widgets/page_menu.py
+++ frontend/widgets/page_menu.py
@@ -2,3 +2,88 @@
 Let side menu for choosing setting for current page
 """
 
+import tkinter as tk
+import customtkinter as ctk
+from CTkMenuBar import *
+
+# TODO: Make settings.py in this file
+
+class page_menu(ctk.CTkTabview):
+    def __init__(self, parent):
+        super().__init__(master = parent)
+
+        # tabs
+        self.add("Start")
+        self.add("Co2")
+        self.add("RelHumid")
+        self.add("Temperature")
+        self.add("AirChange")
+
+        # widgets
+        page_menu_start(self.tab("Start"))
+        page_menu_co2(self.tab("Co2"))
+        page_menu_rh(self.tab("RelHumid"))
+        page_menu_temp(self.tab("Temperature"))
+        page_menu_airch(self.tab("AirChange"))
+        
+class page_menu_start(ctk.CTkFrame):
+    def __init__(self, parent):
+        super().__init__(master = parent, fg_color="transparent")
+        self.pack(expand = True, fill = "both")
+
+        # widgets for start under this
+
+class page_menu_co2(ctk.CTkFrame):
+    def __init__(self, parent):
+        super().__init__(master = parent, fg_color="transparent")
+        self.pack(expand = True, fill = "both")
+
+        # widgets for Co2 under this
+
+class page_menu_rh(ctk.CTkFrame):
+    def __init__(self, parent):
+        super().__init__(master = parent, fg_color="transparent")
+        self.pack(expand = True, fill = "both")
+
+        # widgets for RelHumid under this
+
+class page_menu_temp(ctk.CTkFrame):
+    def __init__(self, parent):
+        super().__init__(master = parent, fg_color="transparent")
+        self.pack(expand = True, fill = "both")
+
+        # widgets for Temperature under this
+
+class page_menu_airch(ctk.CTkFrame):
+    def __init__(self, parent):
+        super().__init__(master = parent, fg_color="transparent")
+        self.pack(expand = True, fill = "both")
+
+        # widgets for AirChange under this
+
+
+
+
+if __name__ == "__main__":  
+    root = ctk.CTk()
+
+    #! TO BE REMOVED - TEMPORARY SETTINGS
+    TITLE_FONT = ctk.CTkFont(family="montserrat", size=20, weight="bold")
+    TEXT_FONT= ctk.CTkFont(family="montserrat", size=12)
+    TITLE_MENU_FONT= ctk.CTkFont(family="montserrat", size=12)
+    PAGE_MENU_FONT = ctk.CTkFont(family="montserrat", size=14, weight="bold")
+    COLOR_1 = "#f6f6f4"
+    COLOR_2 = "#edeae5"
+    COLOR_3 = "#ced9dd"
+    COLOR_4 = "#979fa5"
+    COLOR_5 = "#2f3d4c"
+    COLOR_MODE = "dark"
+
+    ctk.set_appearance_mode(COLOR_MODE)
+
+    root.title("TEST: page_menu")
+    root.geometry("800x300")
+
+    page_menu(root).pack()
+
+    root.mainloop()
--- frontend/widgets/title_menu.py
+++ frontend/widgets/title_menu.py
@@ -1,44 +1,40 @@
 """
-title menu for file, edit, view, and help and so on
+title menu for file, edit, help, report bug and so on
 """
+
+# TODO: Redo the lambda functions so they work properly
+# TODO: Make settings.py in this file
 
 import tkinter as tk
 import customtkinter as ctk
 from CTkMenuBar import *
 
-
-
-
-
-
-class title_menu:
+class title_menu(ctk.CTkFrame):
     def __init__(self, parent):
         super().__init__(parent)
 
-        self.text = ctk.CTkLabel(self, text="Title", font=TITLE_FONT)
-        # # main title menu buttons
-        # self.menu = CTkMenuBar(self)
-        # self.button_file = self.menu.add_cascade("File")
-        # self.button_edit = self.menu.add_cascade("Settings")
-        # self.button_help = self.menu.add_cascade("Help")
-        # self.button_report = self.menu.add_cascade("Report Bug")
+        # title menu
+        self.menu = CTkTitleMenu(parent)
 
-        # # file dropdown menu
-        # self.menu_dropdown_1 = CustomDropdownMenu(widget=self.button_file)
-        # self.menu_dropdown_1.add_option(option="Import Data", command=lambda: print("Importing data"), font=TITLE_MENU_FONT)
-        # self.menu_dropdown_1.add_option(option="Open", command=lambda: print("Opening file"), font=TITLE_MENU_FONT)
-        # self.menu_dropdown_1.add_option(option="Save As", command=lambda: print("Save As"), font=TITLE_MENU_FONT)
-        # self.menu_dropdown_1.add_option(option="Save Report", command=lambda: print("Save Report"), font=TITLE_MENU_FONT)
+        # main title menu buttons
+        self.button_file = self.menu.add_cascade("File")
+        self.button_edit = self.menu.add_cascade("Settings", command=lambda: print("Opening Settings"), font=TITLE_MENU_FONT)
+        self.button_help = self.menu.add_cascade("Help", command=lambda: print("Opening Github Wiki"), font=TITLE_MENU_FONT)
+        self.button_report = self.menu.add_cascade("Report Bug", command=lambda: print("Opening Github"), font=TITLE_MENU_FONT)
 
-
-
+        # file dropdown menu
+        self.menu_dropdown_1 = CustomDropdownMenu(widget=self.button_file)
+        self.menu_dropdown_1.add_option(option="Import Data", command=lambda: print("Importing data"), font=TITLE_MENU_FONT)
+        self.menu_dropdown_1.add_option(option="Open", command=lambda: print("Opening file"), font=TITLE_MENU_FONT)
+        self.menu_dropdown_1.add_option(option="Save As", command=lambda: print("Save As"), font=TITLE_MENU_FONT)
+        self.menu_dropdown_1.add_option(option="Save Report", command=lambda: print("Save Report"), font=TITLE_MENU_FONT)
 
 
 
 if __name__ == "__main__":  
     root = ctk.CTk()
 
-    #! TO BE REMOVED - TEMPORARY
+    #! TO BE REMOVED - TEMPORARY SETTINGS
     TITLE_FONT = ctk.CTkFont(family="montserrat", size=20, weight="bold")
     TEXT_FONT= ctk.CTkFont(family="montserrat", size=12)
     TITLE_MENU_FONT= ctk.CTkFont(family="montserrat", size=12)
@@ -48,10 +44,12 @@ if __name__ == "__main__":
     COLOR_3 = "#ced9dd"
     COLOR_4 = "#979fa5"
     COLOR_5 = "#2f3d4c"
+    COLOR_MODE = "dark"
 
+    ctk.set_appearance_mode(COLOR_MODE)
 
-    root.title(f"TEST: {__doc__}")
-    root.geometry("500x100")
+    root.title("TEST: tile_menu")
+    root.geometry("800x300")
 
     title_menu(root).pack()
 
